cca4790766608fe2d8feffd713722701a0868c88
sangeeta1998 <sangeeta.kakati19@gmail.com>
2023-09-18 15:01:13 +0200

    main.c and timestamps

--- product-mini/platforms/posix/timestamps.h
+++ product-mini/platforms/posix/timestamps.h
@@ -52,13 +52,14 @@ timeduration_t time_since(timestamp_t ts1){
     return ts2-ts1;
 }
 
-void print_timestamp(const char * tag, timestamp_t ts){
+void print_timestamp(const char *runtime, const char *tag, timestamp_t ts) {
     if (!initialised) {
         init_timestamps();
     }
-    fprintf(fd, "WABENCH, %s, %s, timestamp, %llu\n", BENCHMARK, tag, ts);
+    fprintf(fd, "%s\t%s\t%s\ttimestamp\t%llu\n", BENCHMARK, runtime, tag, ts);
     fflush(fd);
 }
+
 
 void print_elapsed_time(const char * tag, timeduration_t time){
     if (!initialised) {
